0c6b401352011997bcfae5efaac6128f6152e658
TIMI MARTINS <timi.martins@pau.edu.ng>
2025-10-18 17:29:40 +0100

    week1 commit

---
+++ WEEK1/first program.cpp
@@ -0,0 +1,12 @@
+// my first program in c++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<  " my first program in c++ and my name is timi and im sooo ";
+
+    return 0;
+}
+
---
+++ WEEK1/second.cpp
@@ -0,0 +1,15 @@
+// my first program in c++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double radius = 6.2;
+    double pi = 3.14;
+    double area;
+    area = radius * radius * pi;
+    cout << "The area is " << area << endl;
+
+    return 0;
+}
